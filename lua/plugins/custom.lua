e3c99c2a9697062acf109a58953208be5b6f6327
Themuuln <zerone.offical@gmail.com>
2024-04-23 22:31:52 +0800

    first commit

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"Exafunction/codeium.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			require("codeium").setup({})
+		end,
+	},
+	{
+		"andweeb/presence.nvim",
+	},
+}
